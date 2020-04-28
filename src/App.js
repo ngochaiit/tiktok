daaf2aebfc75d74860e95b01c2cf55d1c66dc596
Ngọc Hải <mac@MacOnes-MacBook-Pro.local>
2020-04-29 01:41:07 +0700

    tiktok

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TikTokAPI, { getRequestParams } from 'tiktok-api';
+
+// Required - a method that signs the URL with anti-spam parameters
+// You must provide an implementation yourself to successfully make
+// most requests with this library.
+const signURL = async (url, ts, deviceId) => {
+    const as = 'anti-spam parameter 1';
+    const cp = 'anti-spam parameter 2'
+    const mas = 'anti-spam parameter 3';
+    return `${url}&as=${as}&cp=${cp}&mas=${mas}`;
+}
+
+// Required - device parameters
+// You need to source these using a man-in-the-middle proxy such as mitmproxy,
+// CharlesProxy or PacketCapture (Android)
+const params = getRequestParams({
+    device_id: '<device_id>',
+    fp: '<device_fingerprint>',
+    iid: '<install_id>',
+    openudid: '<device_open_udid>',
+});
+
+const api = new TikTokAPI(params, { signURL });
+console.log(api);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div>Hello</div>
     </div>
   );
 }
